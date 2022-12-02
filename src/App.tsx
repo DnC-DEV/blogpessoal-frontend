dc0a6ef9f6d3b2961f9e96b1af03a533b47c7a5e
DnC-DEV <daniel.correa1907@hotmail.com>
2022-12-01 23:10:40 -0300

    Atualização dos arquivos css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './component/static/navbar/Navbar';
 import Footer from './component/static/footer/Footer';
+import Navbar from './component/static/navbar/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+
 
 
 function App() {
@@ -12,12 +14,17 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
       </Routes>
       <Footer />
-    </Router >
+    </Router>
+
   );
 }
 
