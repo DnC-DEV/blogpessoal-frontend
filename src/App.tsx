1a2066f048f68f1118738b6d495e3f94c9e78337
DnC-DEV <daniel.correa1907@hotmail.com>
2022-12-10 13:49:22 -0300

    blogpessoal funcional

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,48 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './component/static/navbar/Navbar';
 import Footer from './component/static/footer/Footer';
-import Home from './pages/home/Home';
-import './App.css';
-import Login from './pages/login/Login';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import './App.css';
+import ListaTema from './component/temas/listatema/ListaTema';
+import ListaPostagem from './component/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './component/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './component/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './component/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './component/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login  />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <ToastContainer />
+      <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/posts" element={<ListaPostagem />} />
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+            <Route path="/formularioTema" element={<CadastroTema />} />
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+          </Routes>
+        </div>
+        <Footer />
 
+      </Router>
+    </Provider>
   );
 }
 
--- src/component/static/navbar/Navbar.tsx
+++ src/component/static/navbar/Navbar.tsx
@@ -1,55 +1,92 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
-import { Box, Button, IconButton, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Box} from '@mui/material';
 import { Link } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom'
 import './Navbar.css'
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { useDispatch } from "react-redux";
+import { addToken } from '../../../store/tokens/actions';
+import {toast} from 'react-toastify';
 
 function Navbar() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    
+    function goLogout(){
+        dispatch(addToken(''));
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
+        navigate('/login')
+    }
+
+    var navbarComponent;
+
+    if(token != ""){
+        navbarComponent = <AppBar position="static">
+        <Toolbar variant="dense">
+            <Box className='cursor'>
+                <Typography variant="h5" color="inherit">
+                    BlogPessoal
+                </Typography>
+            </Box>
+
+            <Box display="flex" justifyContent="start">
+                <Link to="/home" className="text-decorator-none">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit">
+                            home
+                        </Typography>
+                    </Box>
+                </Link>
+                <Link to="/posts" className="text-decorator-none">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit">
+                            postagens
+                        </Typography>
+                    </Box>
+                </Link>
+                <Link to="/temas" className="text-decorator-none">
+                <Box mx={1} className='cursor'>
+                    <Typography variant="h6" color="inherit">
+                        temas
+                    </Typography>
+                </Box>
+                </Link>
+                <Link to="/formularioTema" className="text-decorator-none">
+                <Box mx={1} className='cursor'>
+                    <Typography variant="h6" color="inherit">
+                        cadastrar tema
+                    </Typography>
+                </Box>
+                </Link>
+              
+                    <Box mx={1} className='cursor' onClick={goLogout}>
+                        <Typography variant="h6" color="inherit">
+                            logout
+                        </Typography>
+                    </Box>
+                
+            </Box>
+
+        </Toolbar>
+    </AppBar>
+    }
     return (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            DnC
-                        </Typography>
-                        <Box display="flex" justifyContent="start">
-                            <Box mx={1}>
-                                <Button className='button-nav'>
-                                    HOME
-                                </Button>
-                            </Box>
-                            <Box mx={1}>
-                                <Button className='button-nav'>
-                                    POSTAGENS
-                                </Button>
-                            </Box>
-                            <Box mx={1}>
-                                <Button className='button-nav'>
-                                    TEMA
-                                </Button>
-                            </Box>
-                            <Link to='/login' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Button variant="outlined" color="inherit">
-                                        LOGIN
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-            </Box>
+            {navbarComponent}
         </>
     )
 }
